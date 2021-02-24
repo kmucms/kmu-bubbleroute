f23e26a4216f93b02fa1c76a7e00743dc086f7cb
waleri <waleri@gosalsa.de>
2021-02-24 08:23:20 +0100

    tracing vars cl improved

--- phar/src/kmucms/uipages/VarTrace.php
+++ phar/src/kmucms/uipages/VarTrace.php
@@ -4,20 +4,25 @@ namespace kmucms\uipages;
 
 class VarTrace{
 
-  private $prefix     = '';
-  private $breadcrumb = [];
-  private $values     = [];
+  private $prefix              = '';
+  private $breadcrumb          = [];
+  private $templatesBreadcrumb = [];
+  private $currentTemplate     = '';
+  private $values              = [];
 
   public function setPrefix(string $prefix){
     $this->prefix = $prefix;
   }
 
-  public function pushLevel(string $name){
-    $this->breadcrumb[] = $name;
+  public function pushLevel(string $name, string $currentTemplateId){
+    $this->breadcrumb[]          = $name;
+    $this->templatesBreadcrumb[] = $currentTemplateId;
+    $this->currentTemplate       = $currentTemplateId;
   }
 
   public function popLevel(){
     array_pop($this->breadcrumb);
+    array_pop($this->templatesBreadcrumb);
   }
 
   public function getValue(string $key){
